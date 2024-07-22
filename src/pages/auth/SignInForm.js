c69795b889fcb12310f625ac843a36978eedbea6
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-22 20:58:18 +0000

    handle submit for Signin
    1. Create a form submit handler, which will be an async function called handleSubmit with an event parameter.
    2. Inside, call event.preventDefault() so that the page doesn’t refresh.
    3. Inside, create a try-catch block, we’ll use axios to post all the signInData to the /dj-rest-auth/login/ endpoint
    4. Write the code to redirect the user to the home page after successfully logging in.
    5. On the Form component, add an onSubmit prop and pass it the handleSubmit function.

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
 import Button from "react-bootstrap/Button";
@@ -9,11 +8,14 @@ import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
+
+import axios from "axios";
+
 
 function SignInForm() {
     const [signInData, setSignInData] = useState({
@@ -22,19 +24,31 @@ function SignInForm() {
       });
     const { username, password } = signInData;
 
+    const history = useHistory();
+
     const handleChange = (event) => {
         setSignInData({
           ...signInData,
           [event.target.name]: event.target.value,
         });
-      };
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+          await axios.post("/dj-rest-auth/login/", signInData);
+          history.push("/");
+        } catch (err) {
+        }
+    };
+
 
   return (
     <Row className={styles.Row}>
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign in</h1>
-          <Form>
+          <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="username">
                     <Form.Label className="d-none">Username</Form.Label>
                     <Form.Control 
