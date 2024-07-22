32e92b892dd796958a54a04a560aa0f8f6891e0e
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-22 15:03:39 +0000

    add "active" styling to nav links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -17,11 +17,21 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <NavLink to="/">
+            <NavLink
+                exact
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/"
+              >              
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink to="/signin">
-              <i className="fas fa-sign-in-alt"></i>Sign in
+              <NavLink
+                exact
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/signin"
+              >              <
+                i className="fas fa-sign-in-alt"></i>Sign in
             </NavLink>
             <Nav.Link>
               <i className="fas fa-user-plus"></i>Sign up
