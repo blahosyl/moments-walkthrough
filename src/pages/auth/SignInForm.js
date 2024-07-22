7554d3d85d1808fe97a18da345457fa27cf1ce54
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-22 20:39:52 +0000

    add basic SignIn form

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -15,15 +15,41 @@ import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
 function SignInForm() {
-//   Add your component logic here
+  //   Add your component logic here
 
   return (
     <Row className={styles.Row}>
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign in</h1>
-          {/* Add your form here */}
+          {
+            <Form>
+              <Form.Group controlId="username">
+                <Form.Label className="d-none">Enter username</Form.Label>
+                <Form.Control 
+                    type="text" 
+                    placeholder="Username"
+                    name="username"
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text>
+              </Form.Group>
 
+              <Form.Group controlId="password">
+                <Form.Label className="d-none">Password</Form.Label>
+                <Form.Control 
+                    type="password" 
+                    placeholder="Password"
+                    name="password"
+                />
+              </Form.Group>
+
+              <Button className = {`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`} type="submit">
+                Sign In
+              </Button>
+            </Form>
+          }
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
