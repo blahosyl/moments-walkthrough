a4f1f53152a2e7b68039943a132373924727bef9
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-22 15:00:47 +0000

    add links to logo, Home & Signin in nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,24 +2,27 @@ import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 
 const NavBar = () => {
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
+            <NavLink to="/">
               <i className="fas fa-home"></i>Home
-            </Nav.Link>
-            <Nav.Link>
+            </NavLink>
+            <NavLink to="/signin">
               <i className="fas fa-sign-in-alt"></i>Sign in
-            </Nav.Link>
+            </NavLink>
             <Nav.Link>
               <i className="fas fa-user-plus"></i>Sign up
             </Nav.Link>
