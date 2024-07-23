41bbfd87bcb8e58e5f6917d1f408b71a09bd4518
Sylvia Blaho <sylvia.blaho@gmail.com>
2024-07-23 14:26:53 +0000

    handle access & refresh tokens
    1. create axios interceptors axiosRes & axiosReq
    2. set axiosRes to refresh access_token if it gets an 401 error
    3. add axiosRes to handleMount
    4. set axiosReq to always refresh access token before making a request

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -6,3 +6,6 @@ axios.defaults.baseURL = "https://drl-walkthough-6ebc76335062.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 // prevent errors when sending cookies
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
